7a1cad6ab95eb87060abf4fe8d19986ddb52c488
moonyerim <moonyerim2@gmail.com>
2022-03-16 13:06:56 +0900

    refactor: top-bar.scss 엘리먼트 선택자 대신 클래스 선택자 사용하도록 수정

--- public/components/top-bar/top-bar.js
+++ public/components/top-bar/top-bar.js
@@ -5,15 +5,17 @@ class TopBar extends HTMLElement {
   }
 
   template() {
-    return `<ul class="flex">
-        <li>즐겨찾기</li>
-        <li>입점신청</li>
-      </ul>
-      <ul class="flex">
-        <li>로그인</li>
-        <li>회원가입</li>
-        <li>고객센터</li>
-      </ul>`;
+    return `<div class="top-bar">
+    <ul class="flex">
+    <li class="li-item">즐겨찾기</li>
+    <li class="li-item">입점신청</li>
+    </ul>
+    <ul class="flex">
+    <li class="li-item">로그인</li>
+    <li class="li-item">회원가입</li>
+    <li class="li-item">고객센터</li>
+    </ul>
+    </div>`
   }
 }
 
