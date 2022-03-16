50ad564c2fcd3d359671024b54cbb4f43075df6d
moonyerim <moonyerim2@gmail.com>
2022-03-17 00:11:38 +0900

    fix & rename: body 태그 사이즈에 영향 받지 않도록 수정, top-bar 컴포넌트 js 파일명 수정

---
+++ public/components/top-bar/topBar.js
@@ -0,0 +1,27 @@
+class TopBar extends HTMLElement {
+  constructor() {
+    super();
+    this.innerHTML = this.template();
+    this.style.background = '#f0f0f0';
+  }
+
+  template() {
+    return `<div class="top-bar">
+      <div class="top-bar__inner">
+        <ul class="flex">
+        <li class="li-item">즐겨찾기</li>
+        <li class="li-item">입점신청</li>
+        </ul>
+        <ul class="flex">
+        <li class="li-item">로그인</li>
+        <li class="li-item">회원가입</li>
+        <li class="li-item">고객센터</li>
+        </ul>
+      </div>
+    </div>`
+  }
+}
+
+window.customElements.define('top-bar', TopBar);
+
+export default TopBar;
