14fe1f1676a43dce76df0dca97a61187af320089
moonyerim <moonyerim2@gmail.com>
2022-03-17 00:14:58 +0900

    feat: gnb 컴포넌트 추가

---
+++ public/components/gnb/gnb.js
@@ -0,0 +1,25 @@
+class Gnb extends HTMLElement{
+  constructor() {
+    super();
+    this.innerHTML = this.template();
+  }
+
+  template() {
+    return `<ul class="gnb">
+        <li class="gnb-item"><a href="#"> 로켓배송 </a></li>
+        <li class="gnb-item"><a href="#"> 쿠팡비즈 </a></li>
+        <li class="gnb-item"><a href="#"> 로켓프레시 </a></li>
+        <li class="gnb-item"><a href="#"> 로켓직구 </a></li>
+        <li class="gnb-item"><a href="#"> 골드박스 </a></li>
+        <li class="gnb-item"><a href="#"> 와우회원할인 </a></li>
+        <li class="gnb-item"><a href="#"> 로켓배송 </a></li>
+        <li class="gnb-item"><a href="#"> 이벤트 </a></li>
+        <li class="gnb-item"><a href="#"> 기획전 </a></li>
+        <li class="gnb-item"><a href="#"> 여행/티켓 </a></li>
+      </ul>`;
+  }
+}
+
+window.customElements.define('nav-gnb', Gnb);
+
+export default Gnb;
