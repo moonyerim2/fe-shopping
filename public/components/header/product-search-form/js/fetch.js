6fcff3c6625ac2406fd01ea8bc5118e3edee5493
moonyerim <moonyerim2@gmail.com>
2022-03-19 23:28:15 +0900

    refactor & remove: fetchMatchingData함수 Util클래스로 이동

--- public/components/header/product-search-form/js/fetch.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable arrow-parens */
-/* eslint-disable import/prefer-default-export */
-const fetchMatchingData = url => fetch(url)
-  .then(res => res.json())
-  .then(data => data.suggestions.map(v => v.value));
-
-export { fetchMatchingData };
--- public/components/header/product-search-form/js/searchTermModal.js
+++ public/components/header/product-search-form/js/searchTermModal.js
@@ -4,7 +4,6 @@
 /* eslint-disable import/extensions */
 import { DomUtil } from '../../../../base/js/DomUtil.js';
 import { Util } from '../../../../base/js/util.js';
-import { fetchMatchingData } from './fetch.js';
 
 /* eslint-disable import/prefer-default-export */
 class SearchTermModal {
@@ -23,7 +22,7 @@ class SearchTermModal {
 
   fetchAutoCompleteSearchTerms(inputValue) {
     const url = `https://completion.amazon.com/api/2017/suggestions?session-id=133-4736477-7395454&customer-id=&request-id=4YM3EXKRH1QJB16MSJGT&page-type=Gateway&lop=en_US&site-variant=desktop&client-info=amazon-search-ui&mid=ATVPDKIKX0DER&alias=aps&b2b=0&fresh=0&ks=71&prefix=${inputValue}&event=onKeyPress&limit=11&fb=1&suggestion-type=KEYWORD`;
-    return fetchMatchingData(url);
+    return Util.fetchMatchingData(url);
   }
 
   insertAutoComptetingList(modal, inputValue) {
